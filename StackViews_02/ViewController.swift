ccac9b1420ba3478d3aa197b674d536572c2dcae
mabus <m.buscaroli@outlook.com>
2021-08-29 20:52:07 +0100

    Initial Setup with three Icons.

--- StackViews_02/ViewController.swift
+++ StackViews_02/ViewController.swift
@@ -11,9 +11,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(containerStackView)
+        setupLayout()
     }
-
+    
+    
+    let containerStackView: UIStackView = {
+        let iconConfig = UIImage.SymbolConfiguration(pointSize: 80.0)
+        
+        let container = UIStackView()
+        container.axis = .vertical
+        container.alignment = .center
+        container.distribution = .equalSpacing
+        container.spacing = 30
+        container.translatesAutoresizingMaskIntoConstraints = false
+        
+        let icon1 = UIImageView(image: UIImage(systemName: "plus", withConfiguration: iconConfig))
+        icon1.translatesAutoresizingMaskIntoConstraints = false
+        let icon2 = UIImageView(image: UIImage(systemName: "person", withConfiguration: iconConfig))
+        icon2.translatesAutoresizingMaskIntoConstraints = false
+        let icon3 = UIImageView(image: UIImage(systemName: "printer", withConfiguration: iconConfig))
+        icon3.translatesAutoresizingMaskIntoConstraints = false
+        
+        container.addArrangedSubview(icon1)
+        container.addArrangedSubview(icon2)
+        container.addArrangedSubview(icon3)
+        
+        return container
+    }()
+    
+    func setupLayout() {
+        containerStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        containerStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        containerStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
 
 }
 
